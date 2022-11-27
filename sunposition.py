7c60f61a1ecd090f9e412dd10e78e56b78bebf63
Michael Hirschmugl <michael.hirschmugl@gmx.at>
2022-11-27 19:06:35 +0100

    Output is only sun angle.

--- sunposition.py
+++ sunposition.py
@@ -34,9 +34,9 @@ if __name__ == '__main__':
     parser.add_argument('--version',action='version',version='%(prog)s 1.0')
     parser.add_argument('--citation',dest='cite',action='store_true',help='Print citation information')
     parser.add_argument('-t,--time',dest='t',type=str,default='now',help='"now" or date and time (UTC) in "YYYY-MM-DD hh:mm:ss.ssssss" format or a (UTC) POSIX timestamp')
-    parser.add_argument('-lat,--latitude',dest='lat',type=float,default=51.48,help='latitude, in decimal degrees, positive for north')
-    parser.add_argument('-lon,--longitude',dest='lon',type=float,default=0.0,help='longitude, in decimal degrees, positive for east')
-    parser.add_argument('-e,--elevation',dest='elev',type=float,default=0,help='elevation, in meters')
+    parser.add_argument('-lat,--latitude',dest='lat',type=float,default=47.07930,help='latitude, in decimal degrees, positive for north')
+    parser.add_argument('-lon,--longitude',dest='lon',type=float,default=15.60140,help='longitude, in decimal degrees, positive for east')
+    parser.add_argument('-e,--elevation',dest='elev',type=float,default=467,help='elevation, in meters')
     parser.add_argument('-T,--temperature',dest='temp',type=float,default=14.6,help='temperature, in degrees celcius')
     parser.add_argument('-p,--pressure',dest='p',type=float,default=1013.0,help='atmospheric pressure, in millibar')
     parser.add_argument('-a,--atmos_refract',dest='a',type=float,default=0.5667,help='atmospheric refraction at sunrise and sunset, in degrees')
@@ -773,12 +773,13 @@ def main(args):
         dr='deg'
         if args.rad:
             dr='rad'
-        print("Computing sun position at T = {t} + {dt} s".format(t=args.t, dt=args.dt))
-        print("Lat, Lon, Elev = {lat} deg, {lon} deg, {elev} m".format(lat=args.lat, lon=args.lon, elev=args.elev))
-        print("T, P = {temp} C, {press} mbar".format(temp=args.temp, press=args.p))
-        print("Results:")
-        print("Azimuth, zenith = {az} {dr}, {zen} {dr}".format(az=az,zen=zen,dr=dr))
-        print("RA, dec, H = {ra} {dr}, {dec} {dr}, {h} {dr}".format(ra=ra, dec=dec, h=h, dr=dr))
+        #print("Computing sun position at T = {t} + {dt} s".format(t=args.t, dt=args.dt))
+        #print("Lat, Lon, Elev = {lat} deg, {lon} deg, {elev} m".format(lat=args.lat, lon=args.lon, elev=args.elev))
+        #print("T, P = {temp} C, {press} mbar".format(temp=args.temp, press=args.p))
+        #print("Results:")
+        #print("Azimuth, zenith = {az} {dr}, {zen} {dr}".format(az=az,zen=zen,dr=dr))
+        #print("RA, dec, H = {ra} {dr}, {dec} {dr}, {h} {dr}".format(ra=ra, dec=dec, h=h, dr=dr))
+        print("{az}".format(az=az))
 
 if __name__ == '__main__':
     if args.test:
